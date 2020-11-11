d7d5298f61ac2ecfd618573efa0ba3b8b9ac5852
savoxit04clear <farhanasqolani040701@gmail.com>
2020-11-11 19:26:57 +0700

    git commit

--- index.php
+++ index.php
@@ -12,6 +12,46 @@
         body{
             height:5000px;
         }
+        .flip-card {
+        background-color: transparent;
+        width: 250px;
+        height: 250px;
+        margin:20px;
+        perspective: 1000px;
+        }
+
+        .flip-card-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.6s;
+        transform-style: preserve-3d;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        }
+
+        .flip-card:hover .flip-card-inner {
+        transform: rotateY(180deg);
+        }
+
+        .flip-card-front, .flip-card-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        }
+
+        .flip-card-front {
+        background-color: #bbb;
+        color: black;
+        }
+
+        .flip-card-back {
+        background-color: #2980b9;
+        color: white;
+        transform: rotateY(180deg);
+        }
 
 
     </style>
@@ -97,45 +137,110 @@
         </div>
         <div class="row">
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/ml.jpg" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>Mobile Legends</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/pubg.png" alt="pubg">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>PUBG Mobile</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/ff.png" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>Free Fire</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/cod.jpg" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>Call of Duty</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/aov.png" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>Arena of Valor</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/coc.png" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>Clash of Clans</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/cr.jpg" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>Clash Royale</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-3">
-                <a href="#">
-                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
-                </a>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img style="width:250px;height:250px;" src="images/pes.png" alt="Mobile legends">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>PES E-Football</h1> 
+                            <p>Architect & Engineer</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
+            
         </div>
     </div>
 </section>
