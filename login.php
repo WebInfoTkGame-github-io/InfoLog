77e558a3d01be72c6cca782c66f92661080f52c7
haryo@kusuma <deeharry96@gmail.com>
2020-11-11 20:29:16 +0700

    git commit

--- login.php
+++ login.php
@@ -53,58 +53,100 @@
         color: white;
         transform: rotateY(180deg);
         }
-/* style haryo */
-.box h2{
-    margin: 0 0 30px;
-    padding: 0;
-    color: #fff;
-    text-align: center;
-}
-.box .inputBox {
-    position: relative;
-}
-.box .inputBox input{
-    width: 100%;
-    padding: 10px 0;
-    font-size: 16px;
-    color: #fff;
-    letter-spacing: 1px;
-    margin-bottom: 30px;
-    border: none;
-    border-bottom: 1px solid #fff;
-    outline: none;
-    background: transparent;
-}
-.box .inputBox label {
-    position: absolute;
-    top: 0;
-    left: 0;
-    letter-spacing: 1px;
-    padding: 10px 0;
-    font-size: 16px;
-    color: #fff;
-    pointer-events: none;
-    transition: .5s;
-}
-.box .inputBox input:focus ~ label,
-.box .inputBox input:valid ~ label{
-    top: -18px;
-    left: 0;
-    color: #03a9f4;
-    font-size: 12px;
-}
-.box input [type="submit"]{
-    background: transparent;
-    border: none;
-    outline: none;
-    color: #fff;
-    background: #03a9f4;
-    padding: 10px 20px;
-    cursor: pointer;
-    border-radius: 5px;
-}
-/* end of style */
 
+        .box h2{
+            margin: 0 0 30px;
+            padding: 0;
+            color: #fff;
+            text-align: center;
+        }
+        .box .inputBox {
+            position: relative;
+        }
+        .box .inputBox input{
+            width: 100%;
+            padding: 10px 0;
+            font-size: 16px;
+            color: #fff;
+            letter-spacing: 1px;
+            margin-bottom: 30px;
+            border: none;
+            border-bottom: 1px solid #fff;
+            outline: none;
+            background: transparent;
+        }
+        .box .inputBox label {
+            position: absolute;
+            top: 0;
+            left: 0;
+            letter-spacing: 1px;
+            padding: 10px 0;
+            font-size: 16px;
+            color: #fff;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .box .inputBox input:focus ~ label,
+        .box .inputBox input:valid ~ label{
+            top: -18px;
+            left: 0;
+            color: #03a9f4;
+            font-size: 12px;
+        }
+        .box input [type="submit"]{
+            background: transparent;
+            border: none;
+            outline: none;
+            color: #fff;
+            background: #03a9f4;
+            padding: 10px 20px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+        .hod {
+            font-family: 'Bad Script', cursive;
+            color: #111;
+            font-size: 45px;
+            margin: 0;
+            padding: 0;
+    
+        }
+        .hod span {
+            margin: 0;
+            padding: 0;
+            animation: animate1 4s linear infinite;
+        }
+        .hod span:nth-child(1){
+            animation-delay: 0s;
+        }
+        .hod span:nth-child(2){
+            animation-delay: 0.25s;
+        }
+        .hod span:nth-child(3){
+            animation-delay: 0.5s;
+        }
+        .hod span:nth-child(4){
+            animation-delay: 0.75s;
+        }
+        .hod span:nth-child(5){
+            animation-delay: 1s;
+        }
+        @keyframes animate1 {
+    0%,100%{
+        color: #fff;
+        filter: blur(2px);
+        text-shadow: 0 0 10px blueviolet,
+                     0 0 20px blueviolet,
+                     0 0 40px blueviolet,
+                     0 0 60px blueviolet,
+                     0 0 80px blueviolet;
+                    }
+    5%,95%{
+        color: #111;
+        filter: blur(0px);
+        text-shadow: none;
+    }
+}
     </style>
 
     <title>Web Info Tourney</title>
@@ -174,12 +216,20 @@
         </ul>
         </div>
         </nav>
-	<h1>Membuat Login Anda</h1>
+<div class="w3-container w3-padding-64 box" id="contact">
+	<h1>Membuat 
+    <span class="hod">L</span>
+    <span class="hod">o</span>
+    <span class="hod">g</span>
+    <span class="hod">i</span>
+    <span class="hod">n</span>
+     Anda</h1>
 	<h3>Masukan data:</h3>
 	<form action="login2.php" method="post">		
             <div class="inputBox"><p><input type="email" name="email" required=""><label>Email</label></p></div>
             <div class="inputBox"><p><input type="password" name="password" required=""><label>Password</label></p></div>
             <div class="inputBox"><p><button type="submit" value="Log In" name="login">Log In</button></p></div>
 	</form>
+</div>
 </body>
 </html>
