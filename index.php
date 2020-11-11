de84dd81cc2df7570debf8fd086e31c9443a4eef
savoxit04clear <farhanasqolani040701@gmail.com>
2020-11-11 15:44:16 +0700

    git commit

---
+++ index.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body{
+            height:5000px;
+        }
+
+
+    </style>
+
+    <title>Web Info Tourney</title>
+  </head>
+  <body>
+    <!-- Header -->
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="images/aa.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/r.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/uwu.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+        <!-- Navbar -->
+        <nav class="navbar sticky-top navbar-expand-lg navbar-primary bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <a class="navbar-brand" href="#">
+            <img src="images/sh3.png" alt="logo" style="width:80px;">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" style="color:white" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Tournament</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">E-sport recruitment</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Top-up credit</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Add partner</a>
+            </li>
+        </ul>
+        <ul class="nav nav-pills ml-auto">
+            <li class="nav-item" style="margin-top:7px">
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="margin-top:5px;color:white" href="#"><i style="font-size:24px" class="fa">&#xf2bd;</i> Login</a>
+            </li>
+        </ul>
+        </div>
+        </nav>
+<!-- akhir header -->
+<!-- Content -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="game">
+                    <h2 style="margin-top:20px">Pilih Tournament</h2>
+                    <hr>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+            <div class="col-sm-3">
+                <a href="#">
+                    <img style="width:200px;height:200px; margin:20px" src="images/ml.jpg" alt="Mobile legends">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- akhir content -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
