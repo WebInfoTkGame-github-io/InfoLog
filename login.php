f7885b30a484ce14be95ccc43f6eb39882832af7
haryo@kusuma <deeharry96@gmail.com>
2020-11-11 20:05:18 +0700

    git commit

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
     <style>
         body{
             height:5000px;
@@ -86,13 +87,13 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="index.php">
             <img src="images/sh3.png" alt="logo" style="width:80px;">
         </a>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="nav nav-pills">
             <li class="nav-item">
-                <a class="nav-link active" style="color:white" href="#">Home</a>
+                <a class="nav-link active" style="color:white" href="index.php">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#" style="color:white">Tournament</a>
