61a54b08753da160756bf81222faaabe218f3823
haryo@kusuma <deeharry96@gmail.com>
2020-11-12 14:26:02 +0700

    git commit

---
+++ comment.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- menyisipkan bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+         <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="images/sh.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Web Info Tourney</title>
+  </head>
+  <body class="bg-light">
+    <!-- Header -->
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="images/aa.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/r.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/uwu.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+        <!-- Navbar -->
+        <nav class="navbar sticky-top navbar-expand-lg navbar-primary bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <a class="navbar-brand" href="index.php">
+            <img src="images/sh3.png" alt="logo" style="width:80px;">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" style="color:white" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Tournament</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">E-sport recruitment</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Top-up credit</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color:white">Add partner</a>
+            </li>
+        </ul>
+        <ul class="nav nav-pills ml-auto">
+            <li class="nav-item" style="margin-top:7px">
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="margin-top:5px;color:white" href="login.php"><i style="font-size:24px" class="fa">&#xf2bd;</i> Login</a>
+            </li>
+        </ul>
+        </div>
+        </nav>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-4">
+
+            <div class="card">
+                <div class="card-body text-center">
+
+                    <img class="img img-responsive rounded-circle mb-3" width="160" src="img/<?php echo $_SESSION['user']['photo'] ?>" />
+                    
+                    <h3><?php echo  $_SESSION["user"]["name"] ?></h3>
+                    <p><?php echo $_SESSION["user"]["email"] ?></p>
+
+                    <p><a href="logout.php">Logout</a></p>
+                </div>
+            </div>
+
+            
+        </div>
+
+
+        <div class="col-md-8">
+
+            <form action="" method="post" />
+                <div class="form-group">
+                    <textarea class="form-control" placeholder="Apa yang kamu pikirkan?"></textarea>
+                </div>
+            </form>
+
+            <?php for($i=0; $i < 4; $i++){ ?>
+            <div class="card mb-3">
+                <div class="card-body">
+                Hai !!! Like for this homepage please and join in club info turnament for more information like skill, skin, progress work, or just play game.
+                </div>
+            </div>
+            <?php } ?>
+            
+        </div>
+    
+    </div>
+</div>
+
+</body>
+</html>
