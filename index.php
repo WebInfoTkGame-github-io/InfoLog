790076371302f84938ff205d02b0215a16dd40bd
haryo@kusuma <deeharry96@gmail.com>
2020-11-19 09:32:42 +0700

    git commit

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php 
-session_start();
-if( isset($_SESSION["login"]) ) {
-    header("location: login.php");
-    exit;
-}
-?>
 <!doctype html>
 <html lang="en">
   <head>
--- login.php
+++ login.php
@@ -120,6 +120,51 @@
         text-shadow: none;
     }
 }
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
     </style>
 
     <title>Web Info Tourney</title>
@@ -211,12 +256,18 @@ if( isset($_POST["login"]) ) {
                 <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </li>
-            <li class="nav-item">
-                <select name="login" id="login"><a class="nav-link" style="margin-top:5px;color:white"><i style="font-size:24px" class="fa">&#xf2bd;</i> Login</a>
-                <option value="login"><a href="login.php">login</a></option>
-                <option value="logout"><a href="logout.php">logout</a></option>
-                </select>
-            </li>
+            <div class="nav-item navbar">
+                <div class="dropdown fa" style="font-size:24px">
+                <button class="dropbtn">
+                <i>&#xf2bd;</i>Login
+                <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                <button href="login.php"><a href="#" value="login">login</a></button>
+                <button href="logout.php"><a href="#" value="logout">logout</a></button>
+                </div>
+                </div>
+            </div>
         </ul>
         </div>
         </nav>
